49cbe5149b31a34f89015c42cbd89b4a67e63773
Felexonyango <felexonyango19@gmail.com>
2023-06-20 22:36:19 +0300

    addded List

--- Learn-LINQ/Learn-LINQ/Program.cs
+++ Learn-LINQ/Learn-LINQ/Program.cs
@@ -1,9 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System;
+using System.Collections.Generic;
 class Learn
 {
     static void Main(string[] args)
     {
+        //LIST
+        List<String> food = new List<String>()
+        {
+         food.Add('Orange');
+        food.Add("Tea");
+        food.Add("Pizza");
+    }
+        
+          
+
+       
+        foreach (String data in food)
+        {
+            
+            Console.WriteLine(data);
+        }
+
+
         int[] arrayData = { 1, 2, 3 };
 
         double[] doublesArray = { 0.3, 0.1, 7.0 };
@@ -11,7 +30,7 @@ class Learn
         displayData(arrayData);
         displayData(doublesArray);
       
-
+  
 
     }
     public static void displayData<Tdata>(Tdata[] array)
