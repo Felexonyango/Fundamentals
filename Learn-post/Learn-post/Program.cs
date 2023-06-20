1633c54a8a7a3f8b3c03adace0b9e8f70f7f4caf
Felexonyango <felexonyango19@gmail.com>
2023-06-20 15:32:08 +0300

    updated post  sample project

---
+++ Learn-post/Learn-post/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    public class UserRegistration
+    {
+        private readonly HttpClient httpClient;
+
+        public UserRegistration()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<string> RegisterUserAsync(string name, string email, string password)
+        {
+            var apiUrl = "https://helpdeskapi.sibasi.ltd/user/signupWithEmail"; // Replace with your API endpoint
+
+            var requestData = new
+            {
+                Name = name,
+                Email = email,
+                Password = password
+            };
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(requestData),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await httpClient.PostAsync(apiUrl, content);
+
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            return responseContent;
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+
+            string name = "John Doe";
+            string email = "john.doe@example.com";
+            string password = "pa55w0rd";
+
+            var result = await userRegistration.RegisterUserAsync(name, email, password);
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
